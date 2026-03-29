2be19c5f3c13cb23d6cba12be903cc0ab6f3e264
JenieX <102888858+JenieX@users.noreply.github.com>
2026-03-29 16:16:46 +0300

    Create haraj-immersive.dev.js

---
+++ haraj-immersive.dev.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name           test
+// @version        0.0.0
+// @namespace      https://github.com/JenieX/user-js-work
+// @description    Awesome script!
+// @author         JenieX
+// @match          https://haraj.com.sa/*
+// @grant          none
+// @run-at         document-start
+// @noframes
+// @compatible     chrome Violentmonkey
+// @compatible     edge Violentmonkey
+// @supportURL     https://github.com/JenieX/user-js-work/issues
+// @homepageURL    https://github.com/JenieX/user-js-work/tree/main/haraj-immersive
+// @icon           http://www.google.com/s2/favicons?domain=haraj.com.sa&sz=128
+// @license        MIT
+// ==/UserScript==
+
+alert('test');
